1d0931baa0c783effbfc716ccdecd26a76a1885c
Daniel Weigl <dw@hgs.at>
2012-10-25 22:19:57 +0200

    Add License

--- Main.vb
+++ Main.vb
@@ -1,4 +1,23 @@
-﻿Imports System.IO
+﻿' SQL Extractor
+'   CLI Tool to extract View/StoredProc/...-Definitions from a SQL-Server 
+'   to track them in a versioning system
+'
+' Copyright (C) 2012  Daniel Weigl
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+
+' You should have received a copy of the GNU General Public License
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports System.IO
 Imports System.Collections.Generic
 Imports System.Collections
 
--- README.md
+++ README.md
@@ -2,3 +2,24 @@ SQL Extractor
 =============
 
 CLI Tool to extract View/StoredProc/...-Definitions from an SQL-Server to track them in a versioning system
+
+Command Line
+============
+tbd
+
+
+Configuration Files
+===================
+tbd
+
+
+Examples
+========
+tbd
+
+
+
+License
+=======
+
+GPLv3 - http://www.gnu.org/licenses/gpl-3.0.en.html
